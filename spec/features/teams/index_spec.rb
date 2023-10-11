1ad01814cc0b71278ffe1846aa9b5c7b4f976c3c
Scott DeVoss <scottdevoss@yahoo.com>
2023-10-11 12:16:30 -0600

    completed user story 1

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -7,15 +7,22 @@ RSpec.describe "Team Index", type: :feature do
   # As a visitor
   # When I visit '/teams'
   # Then I see the name of each team record in the system
+
   describe "As a visitor" do
     describe "When I visit '/teams" do
       it "I see the name of each team record in the system" do
         #arrange
-
+        team1 = Team.create(name: "Manchester United", year_founded: 1902, former_champion: true)
+        team2 = Team.create(name: "Liverpool", year_founded: 1892, former_champion: true)
+        
         #act
+        visit "/teams"
 
         #assert
         #save_and_open_page
+        expect(page).to have_content("All Teams")
+        expect(page).to have_content(team1.name)
+        expect(page).to have_content(team2.name)
       end
     end
   end
