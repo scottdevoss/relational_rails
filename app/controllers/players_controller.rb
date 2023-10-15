ece21ed16d38288c0c734d78c8775edb8dd54e86
Scott DeVoss <scottdevoss@yahoo.com>
2023-10-15 14:00:48 -0600

    completed user story 20

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -17,6 +17,12 @@ class PlayersController < ApplicationController
     redirect_to "/players/#{player.id}"
   end
 
+  def destroy
+    player = Player.find(params[:id])
+    player.destroy
+    redirect_to "/players"
+  end
+
   private
   def player_params
     params.permit(:name, :position, :age, :retired)
